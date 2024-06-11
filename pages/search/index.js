48a44670c6ce6ca464a105478f7697f2bb8e709f
Torben Jost <torbenjost@proton.me>
2024-06-11 16:16:27 +0200

    refactor code to move display logic to ArticleCard component

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,11 +1,39 @@
 import ArticleCard from "@/components/Card/Card";
 import SearchBar from "@/components/SearchBar/SearchBarComp";
+import useSWR from "swr";
+import { useState } from "react";
 
 export default function SearchPage() {
+  // States to store date range & language dropdown value
+  const [dateRangeFrom, setDateRangeFrom] = useState("");
+  const [dateRangeTo, setDateRangeTo] = useState("");
+  const [languageValue, setLanguageValue] = useState("");
+  const [keyWord, setKeyword] = useState("");
+
+  // Data fetching
+  const [url, setUrl] = useState(null);
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const { data, error } = useSWR(url, fetcher, { shouldRetryOnError: false });
+  const isLoading = !error && !data && !!url;
+  const handleSearch = () => {
+    setUrl(
+      `https://newsapi.org/v2/everything?q=${keyWord}&from=${dateRangeFrom}&to=${dateRangeTo}&language=${languageValue}&apiKey=21247b89f2cf48c48d0df5ed148af376`
+    );
+  };
   return (
     <>
-      <SearchBar />
-      <ArticleCard />
+      <SearchBar
+        dateRangeFrom={dateRangeFrom}
+        setDateRangeFrom={setDateRangeFrom}
+        dateRangeTo={dateRangeTo}
+        setDateRangeTo={setDateRangeTo}
+        languageValue={languageValue}
+        setLanguageValue={setLanguageValue}
+        keyWord={keyWord}
+        setKeyword={setKeyword}
+        onSearch={handleSearch}
+      />
+      <ArticleCard data={data} error={error} isLoading={isLoading} />
     </>
   );
 }
