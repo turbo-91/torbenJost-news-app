abdedf273f1ba75536f2610b067a7c6b64cdcf86
Torben Jost <torbenjost@proton.me>
2024-06-11 12:45:08 +0200

    write pseudo code to think through next step

--- components/SearchBar/SearchBarComp.jsx
+++ components/SearchBar/SearchBarComp.jsx
@@ -1,12 +1,18 @@
 import DateRangeComp from "./DateRangePicker/DateRangeComp";
 import LanguageDropdown from "./LanguageDropdown/LanguageDropdownComp";
 
+// I need to find a way to
+// get all the form data in the right format
+// to then store them in variables
+// that will be dynamically inserted in the GET URL
+// that I will use to fetch data
+
 export default function SearchBar() {
   return (
     <>
       <DateRangeComp />
       <label className="input" htmlFor="keywords">
-        keywords:
+        type a keyword:
       </label>
       <input id="keywords"></input>
       <LanguageDropdown />
