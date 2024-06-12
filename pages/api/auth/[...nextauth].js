3ce6f0dbe8ce24e521fa0a6154d82f82b1806bfb
Torben Jost <torbenjost@proton.me>
2024-06-12 16:12:49 +0200

    change default session object

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,6 +10,20 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    async session({ session, user }) {
+      // The user object from the database contains the ID of the user in your database
+
+      session.user.userId = user.id;
+
+      // With the code above you can add the user ID to the session object and use it in your pages
+
+      // Make sure you console.log the session and user objects to see what they contain
+
+      return session;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
