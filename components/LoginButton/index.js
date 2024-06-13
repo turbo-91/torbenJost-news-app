fb566b9ce0fbda31283704e340f944a9fb060bab
Torben Jost <torbenjost@proton.me>
2024-06-13 16:04:09 +0200

    add github logos to login button, eliminate seconde page through login process, adjust greeting

--- components/LoginButton/index.js
+++ components/LoginButton/index.js
@@ -1,19 +1,37 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
+import githubLogo from "/public/GitHub_Logo.png";
+import githubMark from "public/github-mark.png";
 
 export default function LoginButton() {
   const { data: session } = useSession();
   if (session) {
     return (
       <>
-        Signed in as {session.user.name} <br />
+        Hey there {session.user.name}! <br />
         <button onClick={() => signOut()}>Sign out</button>
       </>
     );
   }
   return (
     <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      Log in to your account. <br />
+      <button onClick={() => signIn("github")}>
+        <Image
+          src={githubMark}
+          alt="GitHub Logo"
+          width={20}
+          height={20}
+          style={{ marginRight: "8px" }}
+        />
+        <Image
+          src={githubLogo}
+          alt="GitHub Logo"
+          width={20}
+          height={20}
+          style={{ marginRight: "8px" }}
+        />
+      </button>
     </>
   );
 }
