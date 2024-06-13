b8cc6daa0f84c2fefb55458c04b5ff60a34559e9
Torben Jost <torbenjost@proton.me>
2024-06-13 16:12:07 +0200

    add Link to favorites page

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,10 +1,18 @@
 import LoginButton from "@/components/LoginButton";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 export default function LoginPage() {
+  const { data: session, status } = useSession();
   return (
     <>
       <h1>User Area</h1>
       <LoginButton />
+      {session && (
+        <>
+          <Link href={`/favorites`}>See your favorite articles</Link>
+        </>
+      )}
     </>
   );
 }
