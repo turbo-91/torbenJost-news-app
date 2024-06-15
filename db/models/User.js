366386c4795fc3b1ee1bf4b17c021b600fe1a12e
Torben Jost <torbenjost@proton.me>
2024-06-15 16:58:42 +0200

    integrate Article schema into User schema

--- db/models/User.js
+++ db/models/User.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
+  favoriteArticles: [{ type: Schema.Types.ObjectId, ref: "Article" }],
 });
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
